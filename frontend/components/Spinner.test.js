ab7f4c95b5c8be6e7539dfe64303f24fe9a247c7
Rebecca-Stone <primstone16@gmail.com>
2022-04-13 21:45:23 -0500

    cleaning up old code

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Spinner from "./Spinner";
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
